cc3af70bc20f469b5fed49a737e7e7a595f92669
SH Kim <feud72@gmail.com>
2022-06-14 00:41:43 +0900

    taxonomy 모델 작성 Family(과), Genus(속), Species(종) 계층 구조를 models.ForeignKey로 표현

---
+++ taxonomy/admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+
+from . import models
+
+
+@admin.register(models.Family)
+class FamilyAdmin(admin.ModelAdmin):
+    list_display = ["name_kor", "name"]
+    ordering = ["name_kor"]
+
+
+@admin.register(models.Genus)
+class GenusAdmin(admin.ModelAdmin):
+    list_display = ["name", "name_kor", "family", "family_eng"]
+    search_fields = ["family__name", "family__name_kor", "name_kor", "name"]
+    ordering = ["family__name_kor", "name"]
+
+    @admin.display(description="과 영문명")
+    def family_eng(self, obj: models.Species):
+        return "%s" % (obj.family.name)
+
+
+@admin.register(models.Species)
+class SpeciesAdmin(admin.ModelAdmin):
+    readonly_fields = ("family",)
+    list_display = [
+        "name_kor",
+        "family",
+        "scientific_name",
+    ]
+    search_fields = ["genus__name", "name_kor", "scientific_name"]
+    ordering = ["name_kor"]
+
+    @admin.display(description="과")
+    def family(self, obj: models.Species):
+        return "%s" % (obj.genus.family.name_kor)
---
+++ taxonomy/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+
+
+class Family(models.Model):
+    name = models.CharField(verbose_name="과", max_length=100, unique=True)
+    name_kor = models.CharField(
+        verbose_name="과국명", max_length=100, blank=True, null=True
+    )
+
+    class Meta:
+        verbose_name = "Family"
+        verbose_name_plural = "과"
+
+    def __str__(self) -> str:
+        return self.name_kor
+
+
+class Genus(models.Model):
+    family = models.ForeignKey(
+        "taxonomy.Family", on_delete=models.CASCADE, verbose_name="과"
+    )
+    name = models.CharField(verbose_name="속", max_length=100, unique=True)
+    name_kor = models.CharField(
+        verbose_name="속국명", max_length=100, blank=True, null=True
+    )
+
+    class Meta:
+        verbose_name = "Genus"
+        verbose_name_plural = "속"
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Species(models.Model):
+    genus = models.ForeignKey(
+        "taxonomy.Genus", on_delete=models.CASCADE, verbose_name="속"
+    )
+    scientific_name = models.CharField(verbose_name="학명", max_length=100, unique=True)
+    name_kor = models.CharField(
+        verbose_name="국명", max_length=100, blank=True, null=True
+    )
+
+    class Meta:
+        verbose_name = "Species"
+        verbose_name_plural = "종"
+
+    def __str__(self) -> str:
+        return self.scientific_name
